39d2620fc593804ce2e757680b91e734a95a3ad6
Imran Ismail <imran.codely@gmail.com>
2017-09-04 19:17:04 +0800

    Introduce presenter and add time_ago_in_words and distance_of_time_in_words

---
+++ lib/backpack/moment/presenter.ex
@@ -0,0 +1,106 @@
+defmodule Backpack.Moment.Presenter do
+  @minutes_in_year 525600
+  @minutes_in_quarter_year 131400
+  @minutes_in_three_quarters_year 394200
+
+  def time_ago_in_words(from, opts) do
+    unit = Keyword.get(opts, :unit, :seconds)
+    distance_of_time_in_words(from, Backpack.Moment.timestamp(unit), opts)
+  end
+
+  def distance_of_time_in_words(from, to, opts) do
+    unit = Keyword.get(opts, :unit, :seconds)
+    include_seconds = Keyword.get(opts, :include_seconds, false)
+
+    distance_in_seconds =
+      to
+      |> Backpack.Moment.to_unix(unit)
+      |> Kernel.-(Backpack.Moment.to_unix(from, unit))
+      |> System.convert_time_unit(unit, :seconds)
+
+    distance_in_minutes = div(distance_in_seconds, 60)
+
+    if distance_in_minutes <= 1 do
+      if include_seconds do
+        distance_in_seconds_to_words(distance_in_seconds)
+      else
+        "less than a minute"
+      end
+    else
+      with :more_than_a_year <- distance_in_minutes_to_words(distance_in_minutes) do
+        minutes_with_offset = calculate_minutes_offset(from, to, distance_in_minutes)
+        remainder = rem(minutes_with_offset, @minutes_in_year)
+        distance_in_years = div(minutes_with_offset, @minutes_in_year)
+
+        cond do
+          remainder < @minutes_in_quarter_year ->
+            "about #{distance_in_years} years"
+          remainder < @minutes_in_three_quarters_year ->
+            "over #{distance_in_years} years"
+          true ->
+            "almost #{distance_in_years + 1} years"
+        end
+      end
+    end
+  end
+
+  defp distance_in_seconds_to_words(seconds) when seconds < 4,
+    do: "less than 5 seconds"
+  defp distance_in_seconds_to_words(seconds) when seconds < 9,
+    do: "less than 10 seconds"
+  defp distance_in_seconds_to_words(seconds) when seconds < 19,
+    do: "less than 20 seconds"
+  defp distance_in_seconds_to_words(seconds) when seconds < 39,
+    do: "half a minute"
+  defp distance_in_seconds_to_words(seconds) when seconds < 59,
+    do: "less than a minute"
+  defp distance_in_seconds_to_words(_seconds),
+    do: "a minute"
+
+  defp distance_in_minutes_to_words(minutes) when minutes < 45,
+    do: "#{minutes} minutes"
+  defp distance_in_minutes_to_words(minutes) when minutes < 90,
+    do: "about an hour"
+  defp distance_in_minutes_to_words(minutes) when minutes < 1440,
+    do: "about #{div(minutes, 60)} hours"
+  defp distance_in_minutes_to_words(minutes) when minutes < 2520,
+    do: "a day"
+  defp distance_in_minutes_to_words(minutes) when minutes < 43200,
+    do: "#{div(minutes, 1440)} days"
+  defp distance_in_minutes_to_words(minutes) when minutes < 86400,
+    do: "about a month"
+  defp distance_in_minutes_to_words(minutes) when minutes < 525600,
+    do: "#{div(minutes, 43200)} months"
+  defp distance_in_minutes_to_words(minutes) when minutes == 525600,
+    do: "a year"
+  defp distance_in_minutes_to_words(_minutes),
+    do: :more_than_a_year
+
+  defp calculate_minutes_offset(from, to, distance_in_minutes) do
+    if from.__struct__ in [DateTime, NaiveDateTime, Date]
+       && to.__struct__ in [DateTime, NaiveDateTime, Date] do
+      from_year = from.year
+      from_year = if from.month >= 3, do: from_year + 1, else: from_year
+      to_year = to.year
+      to_year = if to.month < 3, do: to_year - 1, else: to_year
+      leap_years =
+        if from_year > to_year do
+          0
+        else
+          from_year..to_year
+          |> Enum.filter(&Calendar.ISO.leap_year?/1)
+          |> Enum.count()
+        end
+
+      minutes_offset_for_leap_year = leap_years * 1440
+      # Discount the leap year days when calculating year distance.
+      # e.g. if there are 20 leap year days between 2 dates having the same day
+      # and month then the based on 365 days calculation
+      # the distance in years will come out to over 80 years when in written
+      # English it would read better as about 80 years.
+      distance_in_minutes - minutes_offset_for_leap_year
+    else
+      distance_in_minutes
+    end
+  end
+end
